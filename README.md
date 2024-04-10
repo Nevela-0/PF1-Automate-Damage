54a47e06dc3fe3234e58eb403e1c86f9f0777548
Nevela-0 <165405732+Nevela-0@users.noreply.github.com>
2024-04-10 04:37:12 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,28 @@
+# PF1 Automate Damage
+Automate Damage is a module for Foundry Virtual Tabletop (FoundryVTT) designed to enhance and streamline the damage calculation and application process for Pathfinder First Edition (PF1) games.
+
+**WARNING: This module is currently in alpha stage. As my first module ever, I have released it to receive feedback and identify potential issues with the way it handles damage. Your feedback and suggestions are highly appreciated to improve the module.**
+
+## Features
+
+- **Damage Calculation**: The module seamlessly integrates into the FoundryVTT interface and PF1E system. It changes the way damage is applied and calculates damage based on various factors, including resistances, immunities, vulnerabilities, and whether the damage source is a spell or weapon. The module always calculates the damage based on the target's damage reduction, resistances, immunities, and vulnerabilities. However, you can always shift-click to apply damage differently as usual with the PF1E system.
+
+## Installation
+Manifest URL: https://github.com/Nevela-0/PF1-Automate-Damage/releases/latest/download/module.json
+
+## Usage
+
+Once the module is installed and enabled, it automatically enhances the damage calculation and application process in your PF1 games without requiring any additional configuration. Simply apply damage as usual, and the module will handle the rest.
+
+## License
+
+This module is licensed under the [MIT License](https://gitlab.com/koboldworks/pf1/little-helper/-/blob/master/LICENSE).
+## Credits
+
+- **Nevela**: Lead developer and creator of the Automate Damage module.
+- **Contributors**: The PF1E system and module developers for their support.
+
+## Acknowledgements
+
+Special thanks to the FoundryVTT community for their support and feedback during the development of this module.
+
---
+++ scripts/main.js
@@ -0,0 +1,171 @@
+Hooks.once('ready', overrideApplyDamage);
+
+function overrideApplyDamage () {
+    libWrapper.register('pf1-automate-damage', 'pf1.documents.item.ItemPF._onChatCardAction', interceptCardData, libWrapper.MIXED);
+    const originalApplyDamage = pf1.documents.actor.ActorPF.applyDamage;
+    libWrapper.register('pf1-automate-damage', 'pf1.documents.actor.ActorPF.applyDamage', function (value, config) {
+        customApplyDamage(originalApplyDamage, value, config);
+        return;
+    }, 'OVERRIDE');
+}
+
+const targetInfo = {
+    id: "",
+    buttonType: ""
+}
+
+function interceptCardData(wrapped, actionName, elementObject) {
+    if(actionName == "applyDamage") {
+        targetInfo.id = elementObject.button.closest('.chat-message').getAttribute('data-message-id');
+        targetInfo.buttonType = elementObject.button.dataset.tooltip;
+    };
+    return wrapped(actionName, elementObject);
+};
+
+function customApplyDamage(originalApplyDamage, value, config) {
+    
+    canvas.tokens.controlled.forEach(token => {
+        let totalDamage = 0;
+        const traits = token.actor.system.traits;
+        const eRes = traits.eres; // Energy resistances
+        const damageImmunities = traits.di; // Damage Immunities
+        const damageReductions = traits.dr; // Damage Reductions
+        const damageVulnerabilities = traits.dv; // Damage Vulnerabilities
+        const messageId = targetInfo.id;
+        const systemRolls = game.messages.get(messageId).systemRolls;
+        const itemSource = game.messages.get(messageId).itemSource;
+        const itemType = game.messages.get(messageId).itemSource.type;
+        const damageMult = targetInfo.buttonType == "PF1.ApplyHalf" ? 0.5 : 1;
+        if (systemRolls?.attacks?.length > 0) {
+            systemRolls.attacks.forEach(attack => {
+                if (attack.damage?.length > 0) {
+                    attack.damage.forEach(damage => {
+                        const damageTypes = damage.options?.damageType?.values || []
+                            if(damageTypes.length < 1) {
+                                damageTypes[0] = "untyped"
+
+                            }
+                            damageTypes.forEach(type => {
+                                let damageForType = Math.floor(damage.total * damageMult)|| 0; // Default to 0 if total damage is not defined
+                                        
+                                if (itemType === "weapon" || (itemType === "attack" && itemSource.system.subType === "weapon")) {
+                                    damageForType = weaponReductionCalculation(itemSource, damageReductions, damageForType, type);
+
+                                } else {
+                                    damageReductions.value.forEach(reduction => {
+                                        if (reduction.types.includes(type)) {
+                                            damageForType -= reduction.amount;
+                                        }
+                                    });
+                                }
+                                    
+                                damageForType = damageImmunityCalculation(damageImmunities, type, damageForType);
+
+                                damageForType = damageVulnerabilityCalculation(damageVulnerabilities, type, damageForType);
+
+                                damageForType = calculateElementalReduction(eRes, type, damageForType);
+
+                                totalDamage += Math.max(0, damageForType);
+                            });
+                    });
+                }
+            });
+        }
+            originalApplyDamage(totalDamage, config);
+    });
+}
+
+function damageImmunityCalculation(damageImmunities, type, damageForType) {
+    if (damageImmunities.value.includes(type)) {
+        damageForType = 0;
+    }
+    return damageForType;
+}
+
+function damageVulnerabilityCalculation(damageVulnerabilities, type, damageForType) {
+    if (damageVulnerabilities.value.includes(type)) {
+        damageForType *= 1.5;
+        damageForType = Math.floor(damageForType);
+    }
+    return damageForType;
+}
+
+function weaponReductionCalculation(itemSource, damageReductions, damageForType, type) {
+    const isMagic = itemSource.system.enh;
+    const hasDR = damageReductions.value.map(reduction => ({
+        amount: reduction.amount,
+        types: reduction.types
+    }));
+    const magicReduction = damageReductions.custom.split(';').find(reduction => reduction.trim().includes("magic"));
+    const epicReduction = damageReductions.custom.split(';').find(reduction => reduction.trim().includes("epic"));
+    const hasMagicReduction = magicReduction && magicReduction.trim().split('/')[1];
+    const hasEpicReduction = epicReduction && epicReduction.trim().split('/')[1];
+
+    if (!isMagic && (type == "slashing" || type == "piercing" || type == "bludgeoning")) {
+        damageForType = nonMagicalWeaponCalculation(hasMagicReduction, hasEpicReduction, magicReduction, epicReduction, damageForType, type, hasDR);
+    } else {
+        damageForType = magicalWeaponCalculation(hasEpicReduction, epicReduction, isMagic, damageForType, type, hasDR);
+    }
+    return damageForType;
+}
+
+function nonMagicalWeaponCalculation(hasMagicReduction, hasEpicReduction, magicReduction, epicReduction, damageForType, type, hasDR) {
+    let reductionAmount = 0;
+    for (let i = 0; i < hasDR.length; i++) {
+        if (!hasDR[i].types.includes(type)) {
+            reductionAmount = hasDR[i].amount;
+            break;
+        }
+    }
+
+    if (hasMagicReduction && hasEpicReduction) {
+        const magicReductionAmount = Math.max(parseInt(magicReduction.trim()), parseInt(epicReduction.trim()));
+        if (!isNaN(magicReductionAmount)) {
+            damageForType -= Math.max(reductionAmount, magicReductionAmount);
+        }
+    } else if (hasMagicReduction) {
+        const magicReductionAmount = parseInt(magicReduction.trim());
+        if (!isNaN(magicReductionAmount)) {
+            damageForType -= Math.max(reductionAmount, magicReductionAmount);
+        }
+    } else if (hasEpicReduction) {
+        const epicReductionAmount = parseInt(epicReduction.trim());
+        if (!isNaN(epicReductionAmount)) {
+            damageForType -= Math.max(reductionAmount, epicReductionAmount);
+        }
+    } else {
+        damageForType -= reductionAmount;
+    }
+    return damageForType;
+}
+
+function magicalWeaponCalculation(hasEpicReduction, epicReduction, isMagic, damageForType, type, hasDR) {
+    let reductionAmount = 0;
+    for (let i = 0; i < hasDR.length; i++) {
+        if (!hasDR[i].types.includes(type)) {
+            reductionAmount = hasDR[i].amount;
+            break;
+        }
+    }
+
+    if (hasEpicReduction && (type == "slashing" || type == "piercing" || type == "bludgeoning")) {
+        const epicReductionAmount = parseInt(epicReduction.trim());
+        if (!isNaN(epicReductionAmount)) {
+            if (epicReductionAmount === 0 || (epicReductionAmount > 0 && isMagic <= 6)) {
+                damageForType -= Math.max(reductionAmount, epicReductionAmount);
+            }
+        }
+    } else {
+        damageForType -= reductionAmount
+    }
+    return damageForType;
+}
+
+function calculateElementalReduction(eRes, type, damageForType) {
+    eRes.value.forEach(resistance => {
+        if (resistance.types.includes(type)) {
+            damageForType = Math.max(0, damageForType - resistance.amount);
+        }
+    });
+    return damageForType;
+}
